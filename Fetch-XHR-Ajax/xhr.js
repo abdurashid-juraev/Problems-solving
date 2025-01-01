00845fcd85c1de3f0f1262558bf89720e01aa49e
abdurashid-juraev <aj6243434@gmail.com>
2025-01-02 04:27:56 +0500

    feat: solved 1 problem

--- Fetch-XHR-Ajax/xhr.js
+++ Fetch-XHR-Ajax/xhr.js
@@ -1,8 +1,48 @@
 const requestUrl = "https://jsonplaceholder.typicode.com/users";
+
+function sendRequest(method, url) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.responseType = "json";
+    xhr.onload = () => {
+      if (xhr.status >= 200 && xhr.status < 300) {
+        resolve(xhr.response);
+      } else {
+        reject(xhr.statusText);
+      }
+    };
+    xhr.onerror = () => {
+      reject(xhr.statusText);
+    };
+    xhr.send();
+  });
+}
+
+sendRequest("GET", requestUrl).then((data) => {
+  // data is the response
+  for (let i = 0; i < data.length; i++) {
+    getData(data[i]);
+  }
+});
+
+function getData(data) {
+  let li = document.createElement("li");
+  document.getElementById("list").appendChild(li);
+  li.innerHTML = `
+    <div>name: ${data.name}</div>
+    <div>email: ${data.email}</div>
+    <div>phone: ${data.phone}</div>
+    <div>username: ${data.username}</div>
+    <div>website: ${data.website}</div>
+    <div>address: ${data.address.city}</div>
+    <div>street: ${data.address.street}</div>
+  `;
+}
+
+/*
 const xhr = new XMLHttpRequest();
-
 xhr.open("GET", requestUrl);
-
 xhr.onload = () => {
   for (let i = 0; i < JSON.parse(xhr.response).length; i++) {
     getData(JSON.parse(xhr.response)[i]);
@@ -10,17 +50,17 @@ xhr.onload = () => {
   }
 };
 xhr.send();
-function getData(n) {
+function getData(data) {
   let li = document.createElement("li");
   document.getElementById("list").appendChild(li);
-
   li.innerHTML = `
-    <div>name: ${n.name}</div>
-    <div>email: ${n.email}</div>
+    <div>name: ${data.name}</div>
+    <div>email: ${data.email}</div>
     <div>phone: ${n.phone}</div>
-    <div>username: ${n.username}</div>
-    <div>website: ${n.website}</div>
-    <div>address: ${n.address.city}</div>
-    <div>street: ${n.address.street}</div>
+    <div>username: ${data.username}</div>
+    <div>website: ${data.website}</div>
+    <div>address: ${data.address.city}</div>
+    <div>street: ${data.address.street}</div>
   `;
 }
+*/
--- rxjs/index.js
+++ rxjs/index.js
@@ -1,28 +1,34 @@
-const { Observable } = require("rxjs");
-const { map } = require("rxjs/operators");
+// const { Observable } = require("rxjs");
+// const { map } = require("rxjs/operators");
+const { interval, take } = require("rxjs");
 const arr = [1, 2, 3, 4, 5, 6, 7, 8];
 
-const observable = new Observable((subscriber) => {
-  subscriber.next(arr);
+// const observable = new Observable((subscriber) => {
+//   subscriber.next(arr);
+// });
+
+// const observer = {
+//   next: (value) => {
+//     console.log("Next " + value);
+//   },
+//   error: (err) => {
+//     console.log("Error " + err);
+//   },
+//   complete: () => {
+//     console.log("Strem is complete");
+//   },
+// };
+
+// observable
+//   .pipe(
+//     map((arr) => {
+//       console.log("map");
+//       return arr.map((item) => item * 2);
+//     })
+//   )
+//   .subscribe(observer);
+
+const source$ = interval(2000);
+source$.pipe(take(8)).subscribe(() => {
+  console.log("hello");
 });
-
-const observer = {
-  next: (value) => {
-    console.log("Next " + value);
-  },
-  error: (err) => {
-    console.log("Error " + err);
-  },
-  complete: () => {
-    console.log("Strem is complete");
-  },
-};
-
-observable
-  .pipe(
-    map((arr) => {
-      console.log("map");
-      return arr.map((item) => item * 2);
-    })
-  )
-  .subscribe(observer);
