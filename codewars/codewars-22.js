8535c71fcd3f7132584669d065c884192ad0bcef
Abdurashid <80646202+abdurashid-juraev@users.noreply.github.com>
2025-01-20 22:53:26 +0500

    solving 2 problems

---
+++ codewars/codewars-22.js
@@ -0,0 +1,24 @@
+/*
+Return the number (count) of vowels in the given string.
+
+We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+The input string will only consist of lower case letters and/or spaces.
+*/
+function getCount(str) {
+  let str2 = "";
+  for (let i = 0; i < str.length; i++) {
+    if (
+      str[i] === "a" ||
+      str[i] === "e" ||
+      str[i] === "i" ||
+      str[i] === "o" ||
+      str[i] === "u"
+    ) {
+      str2 = str2 + str[i];
+    }
+  }
+  console.log(str2.length);
+  return str2.length;
+}
+getCount("abricudobre");
