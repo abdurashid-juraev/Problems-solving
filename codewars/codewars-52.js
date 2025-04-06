1c0ed9492d18077315a6e52cf4d2177f25a39b8a
Abdurashid <80646202+abdurashid-juraev@users.noreply.github.com>
2025-04-06 09:37:55 +0500

    fix merge

---
+++ codewars/codewars-52.js
@@ -0,0 +1,28 @@
+/*Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+
+Examples:
+
+* 'abc' =>  ['ab', 'c_']
+* 'abcdef' => ['ab', 'cd', 'ef']
+*/
+let str = "abcdef";
+let nums = [];
+let odd = [];
+let even = [];
+function solution(str) {
+  // let str = str.split("");
+  for (let i = 0; i <= str.length - 1; i++) {
+    if ((i % 2 === 0) === true) {
+      odd.push(str[i]);
+    }
+    if ((i % 2 === 0) === false) {
+      even.push(str[i]);
+    }
+  }
+
+  console.log(odd);
+  console.log(even);
+
+  return str;
+}
+solution(str);
