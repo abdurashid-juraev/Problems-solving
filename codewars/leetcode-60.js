22aa95660b431ee06abe8dc270fa222b1e884fa9
Abdurashid <80646202+abdurashid-juraev@users.noreply.github.com>
2025-05-26 22:36:43 +0500

    refactor: update number array and enhance logging in findNumbers function; improve HTML structure and styles in MeetingResult.html

--- codewars/leetcode-60.js
+++ codewars/leetcode-60.js
@@ -1,10 +1,11 @@
-const nums = [12, 345, 2, 6, 7896];
+const nums = [345];
 
 let findNumbers = (nums) => {
   let count = 0;
   for (let i = 0; i < nums.length; i++) {
     if (String(nums[i]).length % 2 === 0) {
       count++;
+      console.log(count);
     }
   }
   return count;
