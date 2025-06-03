71272bbb1342a083961465595cce3eaee6e824db
Abdurashid <80646202+abdurashid-juraev@users.noreply.github.com>
2025-06-03 17:18:49 +0500

    refactor: clean up app.js and index.html by removing commented code and unnecessary styles

--- app.js
+++ app.js
@@ -3,14 +3,27 @@
 //}
 //console.log(strStr("sadbutsad", "sado"));
 
-let nums = [1, 2, 4, 6];
+//function fn(n) {
+//  return Math.floor(Math.random() * 20);
+//}
+//console.log(fn(20));
 
-function fn(nums, target) {
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] >= target) {
-      return i;
-    }
-  }
-  return nums.length;
+//function fn(n, d) {
+//  return n.toFixed(d);
+//}
+//console.log(fn(2.1111, 2));
+/*
+8. Most Frequent Array Item
+
+Write a JavaScript program to find the most frequent item in an array.
+
+Sample array : var arr1=[3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
+
+Sample Output : a ( 5 times )
+*/
+async function sleep(millis) {
+  await new Promise((resolve) => setTimeout(resolve, millis));
+  console.log(millis);
+  return millis;
 }
-console.log(fn(nums, 3));
+sleep(100);
