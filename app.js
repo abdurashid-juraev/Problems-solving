e0228692ae08cdd4b3493811697782f78aae01ae
Abdurashid <80646202+abdurashid-juraev@users.noreply.github.com>
2025-06-09 15:52:37 +0500

    refactor: comment out unused functions and enhance object flattening logic

--- app.js
+++ app.js
@@ -21,9 +21,51 @@ Sample array : var arr1=[3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
 
 Sample Output : a ( 5 times )
 */
-async function sleep(millis) {
-  await new Promise((resolve) => setTimeout(resolve, millis));
-  console.log(millis);
-  return millis;
+//async function sleep(millis) {
+//  await new Promise((resolve) => setTimeout(resolve, millis));
+//  console.log(millis);
+//  return millis;
+//}
+//sleep(100);
+//let createCounter = function (init) {
+//  let countValue = init;
+
+//  function increment() {
+//    return ++countValue;
+//  }
+//  function decrement() {
+//    return --countValue;
+//  }
+//  fun
+
+//console.log(chunk(arr, size));
+let obj = {
+  name: "Ali",
+  address: {
+    city: "Tashkent",
+    location: {
+      lat: 41,
+      long: 69,
+    },
+  },
+};
+
+function flattenObject(obj, objKey = "", result = {}) {
+  for (let key in obj) {
+    const newKey = objKey ? `${objKey}.${key}` : key;
+
+    if (
+      typeof obj[key] === "object" &&
+      obj[key] !== null &&
+      !Array.isArray(obj[key])
+    ) {
+      flattenObject(obj[key], newKey, result);
+    } else {
+      result[newKey] = obj[key];
+    }
+  }
+
+  return result;
 }
-sleep(100);
+
+console.log(flattenObject(obj));
